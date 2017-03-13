71b7304f58a25069a3f0d39315d777d5e5e203fd
zhangyongwei <694498731@qq.com>
2017-03-13 08:35:50 +0800

    4.crashHandler类

---
+++ app/src/main/java/com/atguiug/p2pinvest1020/activity/bean/Index.java
@@ -0,0 +1,16 @@
+package com.atguiug.p2pinvest1020.activity.bean;
+
+import android.media.Image;
+
+import java.util.List;
+
+/**
+ * Created by 张永卫on 2017/3/12.
+ */
+
+public class Index {
+
+    public List<Image> imageList;
+
+
+}
--- app/src/main/java/com/atguiug/p2pinvest1020/activity/fragment/HomeFragment.java
+++ app/src/main/java/com/atguiug/p2pinvest1020/activity/fragment/HomeFragment.java
@@ -8,12 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.atguiug.p2pinvest1020.R;
+import com.atguiug.p2pinvest1020.activity.bean.Index;
+import com.atguiug.p2pinvest1020.activity.utils.AppNetConfig;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 /**
  * Created by 张永卫on 2017/3/10.
  */
 
 public class HomeFragment extends Fragment {
+
+    private Index index;
 
     @Nullable
     @Override
@@ -23,6 +29,40 @@ public class HomeFragment extends Fragment {
 
         View view = View.inflate(getActivity(), R.layout.fragment_home,null);
 
+
+        initData();
+
         return view;
+
     }
+
+    private void initData() {
+
+        getDataFromNet();
+    }
+
+    /**
+     * 联网请求数据
+     */
+    private void getDataFromNet() {
+
+        AsyncHttpClient httpClient = new AsyncHttpClient();
+
+        index = new Index();
+
+        httpClient.post(AppNetConfig.INDEX,new AsyncHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(String content) {
+                super.onSuccess(content);
+            }
+
+            @Override
+            public void onFailure(Throwable error, String content) {
+                super.onFailure(error, content);
+            }
+        });
+
+    }
+
 }
